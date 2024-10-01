a319fa3a48118b6887d523b626c5d22c99b81860
kimdan5397 <kimdan5397@gmail.com>
2024-10-01 23:37:00 +0900

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-export { default as GenericView } from './components/genericView';
-export { default as LayoutBuilder } from './components/genericLayout';
-export { useToast } from './hooks/use-toast';
+
 export { createServerClient, createBrowserClient } from './utils/mysingle/createClient';
-export type { GenericViewProps, Field, DashboardCard, ViewType, SelectOption } from './types/genericView';
 export type { ApiConfig, AuthTokens, User, CustomAxiosRequestConfig } from './types/createClient';
+export { apiConfig } from './config/apiConfig';
